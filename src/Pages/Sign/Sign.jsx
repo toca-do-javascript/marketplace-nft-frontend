5eb0c0af51a8291c5c39ad66c8e4659553372135
DevRafaelDeMelo <rafaelmelo765@gmail.com>
2023-06-05 22:14:53 -0300

    Tela de login

--- src/Pages/Sign/Sign.jsx
+++ src/Pages/Sign/Sign.jsx
@@ -1,34 +1,13 @@
 import {  useState } from "react";
 import styled from "styled-components";
 
-   
-
-    const ButtonAba = styled.button`
-        font-size: 20px;
-        color: white;
-        background: none;
-        border: none;
-        padding-bottom: 20px;
-        border-bottom: solid 3px #360636;
-        width: 50%;
-        font-family: sans-serif;
-            &:hover {
-            border-bottom: solid 3px  #ab1aad;
-            }
-    `;
-     
-    const Section = styled.div`
-        width: 50%;
-        margin: auto;
-        margin-top: 40px;
-    `
 
     const Title = styled.div`
         padding-top: 150px;
         text-align: center;
         font-size: 30px;
-        font-family: Arial, Helvetica, sans-serif;
-        color: white;
+        font-family: 'Poppins', sans-serif;
+        color: #0a0a0a;
         font-weight: 500;
         @media only screen and (max-width:700px) {
             padding-top: 50px;
@@ -36,41 +15,30 @@ import styled from "styled-components";
         
     `
 
-    const Coments = styled.div`
-        font-size: 14px;
-        color: white;   
-        text-align: center;
-        width: 50%;
-        margin: auto;
-        font-weight: 400;
-        margin-top: 30px;
-        font-family: sans-serif;
-        @media only screen and (max-width:1000px) {
-            display: none;
-            
-
-            
-        };
-    `    
-
     const MenuLateral = styled.div`
         width: 50%;
         position: fixed;
+        top: 0px;
         right: 0px;
-        background: #181022;
+        background: #F5F5F5;
         height: 100vh;
         @media only screen and (max-width:1000px) {
             width: 100%;
             bottom: 0px;
             height: 70vh;
         };
-    `;
+    `
 
     const FullScream = styled.div`
-        background-image: url("https://th.bing.com/th/id/R.7371bd63196d1f1e7df252abe2eb4b60?rik=zWGiwU2WB4M5Ow&pid=ImgRaw&r=0");
+        background-image: url("https://images.unsplash.com/photo-1645516484419-35a747c99474?crop=entropy&cs=srgb&fm=jpg&ixid=M3w3MjAxN3wwfDF8c2VhcmNofDMwfHxuZnR8ZW58MHx8fHwxNjg1OTg4Nzg5fDA&ixlib=rb-4.0.3&q=85&q=85&fmt=jpg&crop=entropy&cs=tinysrgb&w=450");
+        background-repeat: no-repeat;
+        background-position: center;
         background-size: cover;
+        background: rgb(42,42,42);
+        background: radial-gradient(circle, rgba(42,42,42,1) 0%, rgba(0,0,0,1) 100%);
         height: 100vh;
         width: 50%;
+
         @media only screen and (max-width:1000px) {
             width: 100%;
             background-size: cover;
@@ -84,43 +52,113 @@ import styled from "styled-components";
 
     const TextField = styled.input`
         display: block;
-        width: 50%;
+        width: 40%;
         margin: auto;   
+        background: #d9d9d9;
         padding: 15px;
-        background: #181022;
         margin-bottom: 20px;
         text-align: center;
         font-weight: 700;
-        color:  #ab1aad;
-        border-radius: 50px; 
-        border: solid 1px  #ab1aad;
-        &:hover{
-            border: solid 1px  #ab1aad ;
-        }
-        ::placeholder {
-        color:   #ab1aad
-        }
+        border: none;
+        border-radius: 10px;
     `
     
-    const Button = styled.div`
-        background:  #ab1aad;
-        width: 50%;
+    const ButtonLogin = styled.div`
+        background: #522087;
+        width: 30%;
         color: white;
-        font-size: 14px;
+        font-size: 12px;
         padding: 15px;
         margin: auto;
         text-align: center;
+        margin-top: 40px;
+        font-weight: 800;
         font-family: sans-serif;
-        border-radius: 50px; 
+        border-radius: 10px; 
         letter-spacing: 0.2em;
+        text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    `
 
+    const Span = styled.div`
+        font-weight: 600;
+    `
+
+    const ContaNova = styled.div`
+        margin-top: 30px;
+        font-family: 'Poppins';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 12px;
+        line-height: 24px;
+        text-align: center;
+        letter-spacing: 0.065em;
+    `
+
+    const CopyRigth = styled.div`
+        font-size: 10px;
+        position: absolute;
+        bottom: 10px;
+        width: 100%;
+        text-align: center;
+    `
+
+    const TextInput = styled.div`
+        width: 40%;
+        margin: auto;
+        font-family: 'Poppins';
+        font-style: normal;
+        font-weight: 400;
+        font-size: 16px;
+        line-height: 24px;
+
+letter-spacing: 0.065em;
+    `
+
+    const MenuTitle = styled.div`
+        
+    `
+
+    const TitleLogo = styled.div`
+        padding-top: 300px;
+        width: 100%;
+        padding-left: 15%;
+        font-family: 'Poppins', sans-serif;
+        font-style: normal;
+        font-weight: 700;
+        font-size: 32px;
+        line-height: 48px;
+        color: #531885;
+        text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    `
+
+    const TitleComentario = styled.div`
+        font-family: 'Poppins', sans-serif;
+        font-style: normal;
+        font-weight: 400;
+        font-size: 40px;   
+        margin-top: 10px ;
+        line-height: 53px;
+        padding-left: 15%;
+        color: #FFFFFF;
+
+        text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    `
+
+    const Titlefundo = styled.div`
+        font-family: 'Pavanam', sans-serif;
+        font-style: normal;
+        font-weight: 400;
+        margin-top: 20px;
+        font-size: 17px;
+        line-height: 26px;
+        color: #FFFFFF;
+        padding-left: 15%;
+        text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
     `
 
 const Register = () => {
 
-    const [area, setArea] = useState(true);
     const [values, setValues] = useState("");
-    const [valuesLogin, setValuesLogin] = useState("");
 
     const handleChangeValues = (value) => {
         setValues((prevValue) => ({
@@ -129,77 +167,58 @@ const Register = () => {
         }));
     };
 
-    const handleChangeValuesLogin = (value) => {
-        setValuesLogin((prevValue) => ({
-            ...prevValue,
-            [value.target.name]: value.target.value,
-        }));
-    };
+    
     
     const handleClickButton = () =>{
         console.log(values)
     };
 
-    const handleClickButtonLogin = () =>{
-        console.log(valuesLogin)
-    };
+    
 
     
 
     return (<>
         
        <FullScream>
-        <MenuLateral>
-                <Title>SING IN</Title>
-                <Coments>Connect with us and find the best Nft of the moment, over a thousand most sought after NFTs on the market</Coments>
-                <Section>
-                    <ButtonAba onClick={() => {setArea(!false)}  }>Login</ButtonAba>
-                    <ButtonAba onClick={() => {setArea(!true)}}>Register</ButtonAba>
-                </Section>
-                {area? 
-                    <Login>
-                        
+            <MenuTitle>
+                <TitleLogo>
+                    NFTART
+                </TitleLogo>
+                <TitleComentario>
+                    Create a new world
+                    <br/>
+                    with your creativity.
+                </TitleComentario>
+                <Titlefundo>
+                    Access and check out all the advantages!
+                </Titlefundo>
+
+            </MenuTitle>
+            <MenuLateral>
+                <Title>SIGN IN</Title>
+                <Login>                                
                         <form>
+                            <TextInput>E-mail:</TextInput>
                             <TextField
                                 type="text"
-                                placeholder="Enter you login"
                                 name="loginemail"
                                 onChange={handleChangeValues}
                             />
+                            <TextInput>Senha</TextInput>
                             <TextField
                                 type="text"
-                                placeholder="Enter you Password"
                                 name="loginpassword"
                                 onChange={handleChangeValues}
                             />
-                            <Button onClick={handleClickButton}>Login Acount</Button>
-                        </form>
-                        
-                    </Login>
-                    :
-                    <Login>
-                        <form>
-                            <TextField
-                                type="text"
-                                placeholder="Enter tou E-mail"
-                                name="Remail"
-                                onChange={handleChangeValuesLogin}
-                            />
-                            <TextField
-                                type="text"
-                                placeholder="Enter you Password"
-                                name="Rpassword"
-                                onChange={handleChangeValuesLogin}
-                            />
-                            <TextField
-                                type="text"
-                                placeholder="Repeat you Password"
-                                name="Cpasswor"
-                                onChange={handleChangeValuesLogin}
-                            />
-                            <Button onClick={handleClickButtonLogin}>Create Acount</Button>
-                        </form>    
-                    </Login>}
+                            <ButtonLogin onClick={handleClickButton}>EXPLORE</ButtonLogin>
+                        </form>                                
+                </Login>
+                <ContaNova>
+                    Não tenho conta
+                    <br/>
+                    <Span>Clique aqui</Span>    
+                </ContaNova> 
+                <CopyRigth>Copyrigth  2022 NFTART.All rigths reserved.</CopyRigth>                  
             </MenuLateral>
        </FullScream>
       
