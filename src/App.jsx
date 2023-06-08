38d7b686af1728da79dd222754a6256d9ea1bf6a
Paulo Valleriote <paulomv29@outlook.com>
2023-06-08 19:02:29 -0300

    adicionando rota temporária para o perfil

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Profile from './Pages/Profile/Profile';
 import Register from './Pages/Sign/Sign';
 import Header from './Ui/Components/Header';
 
@@ -9,9 +10,11 @@ const App = () => {
       <Routes>
         <Route path="/header" element={<Header />} />
         <Route path="/" element={<Register />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   );
 };
 
 export default App;
+
