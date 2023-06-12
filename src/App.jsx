339bc5089a16a592e1bebbb997f22f5dce0ff1ad
Paulo Valleriote <paulomv29@outlook.com>
2023-06-12 13:48:16 -0300

    Configurando as rotas do Perfil

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import Profile from './Pages/Profile/Profile';
+import Favorited from './Pages/Profile/components/SubPages/Favorited';
+import MyCollection from './Pages/Profile/components/SubPages/MyCollection';
+import Offers from './Pages/Profile/components/SubPages/Offers';
 import Register from './Pages/Sign/Sign';
 import Header from './Ui/Components/Header';
 
@@ -9,8 +12,13 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/header" element={<Header />} />
-        <Route path="/" element={<Register />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/" element={<Register />}/>
+        <Route path="/profile">
+          <Route path='/profile'  element={<Profile><Offers/></Profile>}/>
+          <Route path='/profile/offers'   element={<Profile><Offers/></Profile>}/>
+          <Route path='/profile/collection'   element={<Profile><MyCollection/></Profile>}/>
+          <Route path='/profile/favorited'  element={<Profile><Favorited/></Profile>}/>
+        </Route>
       </Routes>
     </Router>
   );
