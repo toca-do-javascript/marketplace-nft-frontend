7a2d607e8539a2e4b52271583027b296dc713ebd
Paula Vieira <eipvi@proton.me>
2023-06-15 19:36:17 -0300

    FIX : Footer navigation links

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Register from './Pages/Sign/Sign';
 import Header from './Ui/Components/Header';
+import Footer from './Ui/Components/Footer/Footer';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -10,8 +11,11 @@ const App = () => {
         <Route path="/header" element={<Header />} />
         <Route path="/" element={<Register />} />
       </Routes>
+
+      <Footer />
     </Router>
   );
 };
 
 export default App;
+
