0fe8612d5b99333f3b500323736f804c03df5a44
DevRafaelDeMelo <rafaelmelo765@gmail.com>
2023-06-22 20:25:01 -0300

    Feat: Adptando css para Style componentes

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import Favorited from './Pages/Profile/components/SubPages/Favorited';
 import MyCollection from './Pages/Profile/components/SubPages/MyCollection';
 import Offers from './Pages/Profile/components/SubPages/Offers';
 import Register from './Pages/Sign/Sign';
-import Header from './Ui/Components/Header';
-
 import Main from './Pages/Home/Home';
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
@@ -14,44 +12,12 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/header" element={<Header />} />
-        <Route path="/" element={<Register />} />
-        <Route path="/profile">
-          <Route
-            path="/profile"
-            element={
-              <Profile>
-                <Offers />
-              </Profile>
-            }
-          />
-          <Route
-            path="/profile/offers"
-            element={
-              <Profile>
-                <Offers />
-              </Profile>
-            }
-          />
-          <Route
-            path="/profile/collection"
-            element={
-              <Profile>
-                <MyCollection />
-              </Profile>
-            }
-          />
-          <Route
-            path="/profile/favorited"
-            element={
-              <Profile>
-                <Favorited />
-              </Profile>
-            }
-          />
-        </Route>
+        <Route path="/" element={ <Main />} />
+        <Route path="/profile" element={ <Profile> <Offers /> </Profile> }/>
+        <Route path="/profile/offers" element={ <Profile> <Offers /> </Profile> }/>
+        <Route path="/profile/collection" element={ <Profile> <MyCollection /> </Profile>}/>
+        <Route path="/profile/favorited" element={ <Profile> <Favorited /> </Profile>} />
         <Route path="/register" element={<Register />} />
-        <Route path="/" element={<Main />} />
       </Routes>
     </Router>
   );
--- src/Ui/Components/Footer/Footer.jsx
+++ src/Ui/Components/Footer/Footer.jsx
@@ -1,27 +1,47 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import style from '../../../../Components/Footer/style.css';
-import '../../../../Components/Footer';
-import '../../../../style.css';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import './style.css';
+
+
+const Fuut = styled.div`
+  background: #1E1F28;
+  height: 20vh;
+  padding-top: 100px;
+  text-align: center;
+  color: #FFF;
+  text-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  font-size: 16px;
+  font-family: "DM Sans";
+  line-height: 22px;
+  font-weight: 300;
+  
+`
+const Fuuter = styled.div`
+margin-top: 4%;
+color: white;
+font-family: 'DM Sans';
+font-style: normal;
+font-weight: 400;
+font-size: 13px;
+line-height: 22px;
+`
+
 
 const Footer = () => {
   return (
-    <footer className="{styles.footer}">
-      <nav className="{footer-nav}">
-        <NavLink to="FAQ" end>
-          FAQ
-        </NavLink>
-        <NavLink to="/">Privacy Policy </NavLink>
-        <NavLink to="/">Terms & Conditions </NavLink>
-        <NavLink to="/">Whitepaper</NavLink>
-        <NavLink to="/">Team</NavLink>
-        <NavLink to="/">Collections</NavLink>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/">Market</NavLink>
-        <NavLink to="/">About</NavLink>
-      </nav>
-      <p>Copyright © 2022 NFTART. All rights reserved.</p>
-    </footer>
+    <Fuut>
+      <Link to="/" className='Link--Footer'>FAQ</Link>
+      <Link to="/" className='Link--Footer'>Privacy Policy </Link>
+      <Link to="/" className='Link--Footer'>Terms & Conditions </Link>
+      <Link to="/" className='Link--Footer'>Whitepaper</Link>
+      <Link to="/" className='Link--Footer'>Team</Link>
+      <Link to="/" className='Link--Footer'>Collections</Link>
+      <Link to="/" className='Link--Footer'>Home</Link>
+      <Link to="/" className='Link--Footer'>Market</Link>
+      <Link to="/" className='Link--Footer'>About</Link>
+      <Fuuter >Copyright © 2022 NFTART. All rights reserved.</Fuuter>
+    </Fuut>
   );
 };
 
